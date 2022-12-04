9c51e522755cc14501776454a4a1e59ae292f080
mahnaz nazir <mahnaz.n.ibrahim@gmail.com>
2022-12-04 19:24:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+() => {
+    var companyName = "Scuber";
+    return companyName;
+  };
+  
+  const companyName = "Scuber";
+  let mostProfitableNeighborhood = "Chelsea";
+  let companyCeo = "Susan Smith";
